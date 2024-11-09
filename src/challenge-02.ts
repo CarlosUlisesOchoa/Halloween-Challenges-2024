4093c34d83d17a9ec4ef7a2298d3e88ad8c3df98
Carlos Ochoa <uligibson@hotmail.com>
2024-11-09 13:00:18 -0600

    Implement solution for challenge-02, add comprehensive tests in challenge-02.test.js

--- src/challenge-02.ts
+++ src/challenge-02.ts
@@ -1,1 +1,22 @@
-// Not solved yet 🥲
+export function battleHorde(zombies: string, humans: string) {
+  // Code here
+  let accumulatedZombiePoints = 0
+  let accumulatedHumanPoints = 0
+  for (let i = 0; i < zombies.length; i++) {
+    const zBattlePoints = Number(zombies[i]) + accumulatedZombiePoints
+    const hBattlePoints = Number(humans[i]) + accumulatedHumanPoints
+    const battleResult = Math.abs(zBattlePoints - hBattlePoints)
+    accumulatedZombiePoints = 0
+    accumulatedHumanPoints = 0
+    if (zBattlePoints > hBattlePoints) {
+      accumulatedZombiePoints += battleResult
+    } else if (zBattlePoints < hBattlePoints) {
+      accumulatedHumanPoints += battleResult
+    }
+  }
+  if (accumulatedZombiePoints === accumulatedHumanPoints) return 'x'
+  if (accumulatedZombiePoints > accumulatedHumanPoints) {
+    return `${accumulatedZombiePoints}z`
+  }
+  return `${accumulatedHumanPoints}h`
+}
